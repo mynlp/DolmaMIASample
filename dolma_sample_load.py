c2d2ec948bdba51ec40ca7c41ae7dfdcbe773315
PososAgapo <bchen@kth.se>
2025-01-29 12:47:50 +0900

    debug the Dolma results

--- dolma_sample_load.py
+++ dolma_sample_load.py
@@ -35,8 +35,11 @@ def collate_fn(batch):
     return torch.stack(batch, dim=0)  # [batch_size, seq_len]
 
 if __name__ == "__main__":
-    file_path = "c4_en_valid.npy"
-
+    file_list = ["c4_en_valid.npy", "dolma_book_valid.npy", "m2d2_s2orc_valid.npy", "pile_valid.npy",
+                 "stack_valid.npy", "wikipedia_v103_valid.npy", "common_crawl_valid.npy", "ice_valid.npy",
+                 "pes2o_valid.npy", "reddit_valid.npy", "wiki_valid.npy"]
+    root_file = "data_OLMo2_13b_1124/eval_data/"
+    file_path = root_file + file_list[0]
     dataset = MemmapTokenDataset(file_path=file_path, seq_len=128, dtype="uint32")
     dataloader = DataLoader(
         dataset=dataset,
