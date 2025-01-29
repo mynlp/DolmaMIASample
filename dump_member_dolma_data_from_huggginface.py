813474d11b4c2c9e8dfac6a86d49e167eb7ec3d6
PososAgapo <bchen@kth.se>
2025-01-29 19:47:18 +0900

    dump dolma member data

--- dump_member_dolma_data_from_huggginface.py
+++ dump_member_dolma_data_from_huggginface.py
@@ -1,4 +1,6 @@
 from datasets import load_dataset
+import numpy as np
+from transformers import AutoTokenizer
 
 def batched_data(dataset, batch_size):
     batch = []
@@ -15,6 +17,18 @@ print(dataset)
 
 batch_size = 32
 
-# Use the batched_data function to iterate over the dataset in batches
+file_path = '/work/gk77/share/DolmaMIASample/train_data/raw_data/pes2o.npy'
+estimated_total_samples = 100000
+fp = np.memmap(file_path, dtype='uint32', mode='w+', shape=(estimated_total_samples, 128))
+tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-2-1124-13B")
+seq_len = 128
+offset = 0
 for batch in batched_data(dataset, batch_size):
-     [x["text"] for x in batch]
+    texts = [x["text"] for x in batch]
+    encoded_inputs = tokenizer(texts, padding='max_length', truncation=True, max_length=seq_len,
+                               return_tensors='np')
+    batch_size_actual = len(encoded_inputs['input_ids'])
+    fp[offset:offset + batch_size_actual] = encoded_inputs['input_ids']
+    offset += batch_size_actual
+
+fp.flush()
