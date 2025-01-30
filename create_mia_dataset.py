19b2099bb20d1a32fa189a575b7f472e1193a9e3
PososAgapo <bchen@kth.se>
2025-01-30 21:39:49 +0900

    fix

--- create_mia_dataset.py
+++ create_mia_dataset.py
@@ -70,155 +70,147 @@ def load_text_dataset(filename, directory="saved_datasets"):
     print(f"Loaded dataset from {file_path}")
     return text_dataset
 
-
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--list", type=int, default=1)
 parser.add_argument("--batch_size", type=int, default=100)
 parser.add_argument("--sample_size", type=int, default=1000)
 parser.add_argument("--select_method", type=str, default="truncate", choices=["truncate", "nontruncate"])
 parser.add_argument("--relative_length", type=str, default="False")
+parser.add_argument("--domain", type=str, default="arxiv")
+parser.add_argument("--device", type=int, default="beyondai")
 args = parser.parse_args()
-device = "beyondai" #chomusuke, beyondai
-if device == "wisteria":
+if args.device == "wisteria":
     prefix = "."
-elif device == "chomusuke":
+elif args.device == "chomusuke":
     prefix = "data/bwchen"
-elif device == "beyondai":
+elif args.device == "beyondai":
     prefix = "/store/Dolma"
-
 tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-2-1124-13B")
 seed_list = [10345, 19238, 19093]
-#I will directly corpy "pile arxiv"
 #data_list = ["code search net", "dolma wiki", "dolma stack", "m2d2", "arxiv", "open-web-math", "algebraic-stack"]
 data_list = ["arxiv"]
 length_list = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, "rest"]
 enumerate_length = len(length_list)
 sample_num = 200000
 for idx, seed in enumerate(seed_list):
-    #fix the random seed
     random.seed(seed)
-    for domain in data_list:
-        if domain == "code search net":
-            dataset = load_dataset("code-search-net/code_search_net")
-            member_dataset = dataset["train"]
-            valid_dataset = dataset["validation"]
-            test_dataset = dataset["test"]
-            #merge valid and test
-            non_member_dataset = concatenate_datasets([valid_dataset, test_dataset])
-        elif domain == "dolma wiki":
-            member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/wiki_train.npy"
-            non_member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/wiki_valid.npy"
-        elif domain == "dolma pes2o":
-            member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/pes2o_train.npy"
-            non_member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/pes2o_valid.npy"
-        elif domain == "algebraic-stack":
-            if device == "wisteria":
-                dataset = load_dataset("EleutherAI/proof-pile-2", "algebraic-stack")
-            else:
-                dataset = load_dataset("EleutherAI/proof-pile-2", "algebraic-stack", cache_dir=f"{prefix}")
-            member_dataset = dataset["train"]
-            valid_dataset = dataset["validation"]
-            test_dataset = dataset["test"]
-            if os.path.exists(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}"):
-                member_dataset = load_from_disk(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-            else:
-                random_indices = random.sample(range(len(member_dataset)),
-                                               k=sample_num if sample_num < len(member_dataset) else len(
-                                                   member_dataset))
-                member_dataset = member_dataset.select(random_indices)
-                os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}", exist_ok=True)
-                member_dataset.save_to_disk(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-                member_dataset = load_from_disk(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-            #merge valid and test
-            validation_sampled = valid_dataset
-            test_sampled = test_dataset
-            non_member_dataset = concatenate_datasets([validation_sampled, test_sampled])
-        elif domain == "arxiv":
-            if device == "wisteria":
-                dataset = load_dataset("EleutherAI/proof-pile-2", "arxiv")
-            else:
-                dataset = load_dataset("EleutherAI/proof-pile-2", "arxiv", cache_dir=f"{prefix}")
-            member_dataset = dataset["train"]
-            valid_dataset = dataset["validation"]
-            test_dataset = dataset["test"]
-            if os.path.exists(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}"):
-                member_dataset = load_from_disk(
-                    f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-            else:
-                random_indices = random.sample(range(len(member_dataset)),
-                                               k=sample_num if sample_num < len(member_dataset) else len(
-                                                   member_dataset))
-                member_dataset = member_dataset.select(random_indices)
-                os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}",
-                            exist_ok=True)
-                member_dataset.save_to_disk(
-                    f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-                member_dataset = load_from_disk(
-                    f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-            # merge valid and test
-            validation_sampled = valid_dataset
-            test_sampled = test_dataset
-            non_member_dataset = concatenate_datasets([validation_sampled, test_sampled])
-        elif domain == "open-web-math":
-            if device == "wisteria":
-                dataset = load_dataset("EleutherAI/proof-pile-2", "open-web-math")
-            else:
-                dataset = load_dataset("EleutherAI/proof-pile-2", "open-web-math", cache_dir=f"{prefix}")
-            member_dataset = dataset["train"]
-            valid_dataset = dataset["validation"]
-            test_dataset = dataset["test"]
-            if os.path.exists(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}"):
-                member_dataset = load_from_disk(
-                    f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-            else:
-                random_indices = random.sample(range(len(member_dataset)),
-                                               k=sample_num if sample_num < len(member_dataset) else len(
-                                                   member_dataset))
-                member_dataset = member_dataset.select(random_indices)
-                os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}",
-                            exist_ok=True)
-                member_dataset.save_to_disk(
-                    f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-                member_dataset = load_from_disk(
-                    f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{domain}/raw_data/{seed}")
-            # merge valid and test
-            validation_sampled = valid_dataset
-            test_sampled = test_dataset
-            non_member_dataset = concatenate_datasets([validation_sampled, test_sampled])
-        for i in range(enumerate_length):
-            print (f"Processing {domain} with length {length_list[i]}")
-            if length_list[i] == 0:
-                min_length = 5
-                max_length = length_list[i + 1]
-            elif length_list[i] == "rest":
-                continue
-                #min_length = 1000
-                #max_length = 100000000000
-            else:
-                min_length = length_list[i]
-                max_length = min_length + 100
-            if domain in ["dolma wiki", "dolma pes2o"]:
-                member_dataset = MemmapTokenDataset(member_dataset_path, seq=max_length, dtype="uint32")
-                non_member_dataset = MemmapTokenDataset(non_member_dataset_path, seq=max_length, dtype="uint32")
-                filtered_member_data = load_and_filter_npy_data(member_dataset, args)
-                filtered_nonmember_data = load_and_filter_npy_data(non_member_dataset, args)
-            else:
-                filtered_member_data = load_and_filter_data(member_dataset, min_length, max_length, args, domain)
-                filtered_nonmember_data = load_and_filter_data(non_member_dataset, min_length, max_length, args, domain)
-            member_data = []
-            nonmember_data = []
-            member_data.extend(filtered_member_data)
-            nonmember_data.extend(filtered_nonmember_data)
-            train_dataset = Dataset.from_dict({"data": member_data})
-            test_dataset_short = Dataset.from_dict({"data": nonmember_data})
-            dataset = DatasetDict({
-                'member': train_dataset,
-                'nonmember': test_dataset_short,
-            })
-            os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{min_length}_{max_length}_truncated/{domain}",
+    if args.domain == "code search net":
+        dataset = load_dataset("code-search-net/code_search_net")
+        member_dataset = dataset["train"]
+        valid_dataset = dataset["validation"]
+        test_dataset = dataset["test"]
+        #merge valid and test
+        non_member_dataset = concatenate_datasets([valid_dataset, test_dataset])
+    elif args.domain == "dolma wiki":
+        member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/wiki_train.npy"
+        non_member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/wiki_valid.npy"
+    elif args.domain == "dolma pes2o":
+        member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/pes2o_train.npy"
+        non_member_dataset_path = "data_OLMo2_13b_1124/train_data/raw_data/pes2o_valid.npy"
+    elif args.domain == "algebraic-stack":
+        if args.device == "wisteria":
+            dataset = load_dataset("EleutherAI/proof-pile-2", "algebraic-stack")
+        else:
+            dataset = load_dataset("EleutherAI/proof-pile-2", "algebraic-stack", cache_dir=f"{prefix}")
+        member_dataset = dataset["train"]
+        valid_dataset = dataset["validation"]
+        test_dataset = dataset["test"]
+        if os.path.exists(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}"):
+            member_dataset = load_from_disk(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+        else:
+            random_indices = random.sample(range(len(member_dataset)),
+                                           k=sample_num if sample_num < len(member_dataset) else len(
+                                               member_dataset))
+            member_dataset = member_dataset.select(random_indices)
+            os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}", exist_ok=True)
+            member_dataset.save_to_disk(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+            member_dataset = load_from_disk(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+        #merge valid and test
+        validation_sampled = valid_dataset
+        test_sampled = test_dataset
+        non_member_dataset = concatenate_datasets([validation_sampled, test_sampled])
+    elif args.domain == "arxiv":
+        if arg.device == "wisteria":
+            dataset = load_dataset("EleutherAI/proof-pile-2", "arxiv")
+        else:
+            dataset = load_dataset("EleutherAI/proof-pile-2", "arxiv", cache_dir=f"{prefix}")
+        member_dataset = dataset["train"]
+        valid_dataset = dataset["validation"]
+        test_dataset = dataset["test"]
+        if os.path.exists(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}"):
+            member_dataset = load_from_disk(
+                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+        else:
+            random_indices = random.sample(range(len(member_dataset)),
+                                           k=sample_num if sample_num < len(member_dataset) else len(
+                                               member_dataset))
+            member_dataset = member_dataset.select(random_indices)
+            os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}",
                         exist_ok=True)
-            dataset.save_to_disk(
-                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{min_length}_{max_length}_truncated/{domain}")
+            member_dataset.save_to_disk(
+                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+            member_dataset = load_from_disk(
+                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+        # merge valid and test
+        validation_sampled = valid_dataset
+        test_sampled = test_dataset
+        non_member_dataset = concatenate_datasets([validation_sampled, test_sampled])
+    elif args.domain == "open-web-math":
+        if args.device == "wisteria":
+            dataset = load_dataset("EleutherAI/proof-pile-2", "open-web-math")
+        else:
+            dataset = load_dataset("EleutherAI/proof-pile-2", "open-web-math", cache_dir=f"{prefix}")
+        member_dataset = dataset["train"]
+        valid_dataset = dataset["validation"]
+        test_dataset = dataset["test"]
+        if os.path.exists(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}"):
+            member_dataset = load_from_disk(
+                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+        else:
+            random_indices = random.sample(range(len(member_dataset)),
+                                           k=sample_num if sample_num < len(member_dataset) else len(
+                                               member_dataset))
+            member_dataset = member_dataset.select(random_indices)
+            os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}",
+                        exist_ok=True)
+            member_dataset.save_to_disk(
+                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+            member_dataset = load_from_disk(
+                f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{args.domain}/raw_data/{seed}")
+        # merge valid and test
+        validation_sampled = valid_dataset
+        test_sampled = test_dataset
+        non_member_dataset = concatenate_datasets([validation_sampled, test_sampled])
+    for i in range(enumerate_length):
+        print (f"Processing {args.domain} with length {length_list[i]}")
+        if length_list[i] == 0:
+            min_length = 5
+            max_length = length_list[i + 1]
+        elif length_list[i] == "rest":
+            continue
+        else:
+            min_length = length_list[i]
+            max_length = min_length + 100
+        if args.domain in ["dolma wiki", "dolma pes2o"]:
+            member_dataset = MemmapTokenDataset(member_dataset_path, seq=max_length, dtype="uint32")
+            non_member_dataset = MemmapTokenDataset(non_member_dataset_path, seq=max_length, dtype="uint32")
+            filtered_member_data = load_and_filter_npy_data(member_dataset, args)
+            filtered_nonmember_data = load_and_filter_npy_data(non_member_dataset, args)
+        else:
+            filtered_member_data = load_and_filter_data(member_dataset, min_length, max_length, args, args.domain)
+            filtered_nonmember_data = load_and_filter_data(non_member_dataset, min_length, max_length, args, args.domain)
+        member_data = []
+        nonmember_data = []
+        member_data.extend(filtered_member_data)
+        nonmember_data.extend(filtered_nonmember_data)
+        train_dataset = Dataset.from_dict({"data": member_data})
+        test_dataset_short = Dataset.from_dict({"data": nonmember_data})
+        dataset = DatasetDict({
+            'member': train_dataset,
+            'nonmember': test_dataset_short,
+        })
+        os.makedirs(f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{min_length}_{max_length}_truncated/{args.domain}",
+                    exist_ok=True)
+        dataset.save_to_disk(
+            f"{prefix}/dolma_absolute_filtered_dataset_{idx + 1}/{min_length}_{max_length}_truncated/{args.domain}")
 
