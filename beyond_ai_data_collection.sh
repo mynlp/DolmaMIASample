3047b97b2238f2720dd5363e4f8077a02872354e
PososAgapo <bchen@kth.se>
2025-02-01 14:26:52 +0900

    fix bug

---
+++ beyond_ai_data_collection.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=datacollection
+#SBATCH --partition=010-partition
+#SBATCH --gres=gpu:1              # 请求8个GPU
+#SBATCH --nodes=1                  # 确保在一个节点上运行
+#SBATCH --ntasks=8                 # 并行启动8个任务
+#SBATCH --output=datacollection.log
+#SBATCH --error=datacollection.err
+#SBATCH --cpus-per-task=8          # 每个任务使用的CPU数量
+
+
+# 定义并行化函数
+python create_mia_dataset.py  --device beyondai --domain arxiv --batch_size 10
